3bc5d06dadfa5ebb3630de09dce98822e5f0cff1
Artur Iatsko <arturyatsko@gmail.com>
2022-08-21 16:26:52 -0400

    feat(footer): add component and story

---
+++ components/Footer/Footer.story.tsx
@@ -0,0 +1,12 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Footer from './Footer';
+
+export default {
+  title: 'Footer',
+  component: Footer,
+} as ComponentMeta<typeof Footer>;
+
+const Template: ComponentStory<typeof Footer> = () => <Footer />;
+
+export const Default = Template.bind({});
+Default.args = {};
---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function AppFooter() {
+  return <div>AppFooter</div>;
+}
