43a67656b67b6904e3cb778054e7882e27042f5f
Artur Iatsko <arturyatsko@gmail.com>
2022-08-21 16:34:06 -0400

    feat(footer): add footer content

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,14 @@
+import { Container, Footer, Text } from '@mantine/core';
 import React from 'react';
 
 export default function AppFooter() {
-  return <div>AppFooter</div>;
+  return (
+    <Footer height={50}>
+      <Container>
+        <Text align="center" color="dimmed" size="sm">
+          Copyright &copy; {new Date().getFullYear()} BikeFinderApp
+        </Text>
+      </Container>
+    </Footer>
+  );
 }
