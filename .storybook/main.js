ca7c432f4f340a0aa115e857237679ba655321a9
Artur Iatsko <arturyatsko@gmail.com>
2022-08-25 17:19:18 -0400

    feat: install storybook addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,8 @@ module.exports = {
   stories: ['../**/*.story.mdx', '../**/*.story.@(js|jsx|ts|tsx)'],
   addons: [
     'storybook-dark-mode',
+    '@storybook/addon-actions',
+    '@storybook/addon-viewport',
     {
       name: 'storybook-addon-turbo-build',
       options: { optimizationLevel: 2 },
