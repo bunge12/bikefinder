d64eeee1b951794832d902365b0098c8ec703012
Artur Iatsko <arturyatsko@gmail.com>
2022-10-02 17:31:48 -0400

    feat(install): add switcher between os

--- pages/install.tsx
+++ pages/install.tsx
@@ -1,8 +1,12 @@
-import { Container, List, Title, Image } from '@mantine/core';
+import { Container, List, Title, Image, SegmentedControl, Center } from '@mantine/core';
 import { NextSeo } from 'next-seo';
+import { useState } from 'react';
+import AppFooter from '../components/Footer/Footer';
 import AppHeader from '../components/Header/Header';
 
 export default function InstallPage() {
+  const [platform, setPlatform] = useState('ios');
+
   return (
     <>
       <NextSeo
@@ -20,35 +24,50 @@ export default function InstallPage() {
         <Title order={5} align="center">
           How to install BikeFinder on your device?
         </Title>
-        <List type="ordered" withPadding spacing="sm" pt="md">
-          <List.Item>Open https://bikefinder.app in Chrome</List.Item>
-          <List.Item>
-            Click &quot;Install/Add to Home Screen&quot; at the bottom of the page
-            <Image src="/screens/android-1.png" />
-          </List.Item>
-          <List.Item>
-            Alternatively, click on the menu icon and click &quot;Install App&quot;
-            <Image src="/screens/android-2.png" />
-          </List.Item>
-          <List.Item>
-            Confirm installation
-            <Image src="/screens/android-3.png" />
-          </List.Item>
-          <List.Item>✅ The app will be added to your home screen</List.Item>
-        </List>
-        <List type="ordered" withPadding spacing="sm" pt="md">
-          <List.Item>Open https://bikefinder.app in Safari</List.Item>
-          <List.Item>
-            Click on &quot;Share&quot; icon
-            <Image src="/screens/ios-1.PNG" />
-          </List.Item>
-          <List.Item>
-            Select &quot;Add to Home Screen&quot;
-            <Image src="/screens/ios-2.PNG" />
-          </List.Item>
-          <List.Item>✅ The app will be added to your home screen</List.Item>
-        </List>
+        <Center mt="md">
+          <SegmentedControl
+            data={[
+              { label: 'iPhone', value: 'ios' },
+              { label: 'Android', value: 'android' },
+            ]}
+            value={platform}
+            onChange={setPlatform}
+          />
+        </Center>
+        {platform === 'android' && (
+          <List type="ordered" withPadding spacing="sm" pt="md">
+            <List.Item>Open https://bikefinder.app in Chrome</List.Item>
+            <List.Item>
+              Click &quot;Install/Add to Home Screen&quot; at the bottom of the page
+              <Image src="/screens/android-1.png" />
+            </List.Item>
+            <List.Item>
+              Alternatively, click on the menu icon and click &quot;Install App&quot;
+              <Image src="/screens/android-2.png" />
+            </List.Item>
+            <List.Item>
+              Confirm installation
+              <Image src="/screens/android-3.png" />
+            </List.Item>
+            <List.Item>✅ The app will be added to your home screen</List.Item>
+          </List>
+        )}
+        {platform === 'ios' && (
+          <List type="ordered" withPadding spacing="sm" pt="md">
+            <List.Item>Open https://bikefinder.app in Safari</List.Item>
+            <List.Item>
+              Click on &quot;Share&quot; icon
+              <Image src="/screens/ios-1.PNG" />
+            </List.Item>
+            <List.Item>
+              Select &quot;Add to Home Screen&quot;
+              <Image src="/screens/ios-2.PNG" />
+            </List.Item>
+            <List.Item>✅ The app will be added to your home screen</List.Item>
+          </List>
+        )}
       </Container>
+      <AppFooter />
     </>
   );
 }
