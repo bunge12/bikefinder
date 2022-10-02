3e239ffdd226b3a9edad2334c257ce4b15d09cf0
Artur Iatsko <arturyatsko@gmail.com>
2022-10-02 15:01:09 -0400

    feat(msw): add icons and manifest

--- next.config.js
+++ next.config.js
@@ -2,13 +2,19 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
-  reactStrictMode: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
+const withPWA = require('next-pwa')({
+  dest: 'public',
 });
+
+module.exports = withPWA(
+  withBundleAnalyzer({
+    reactStrictMode: false,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    i18n: {
+      locales: ['en'],
+      defaultLocale: 'en',
+    },
+  })
+);
