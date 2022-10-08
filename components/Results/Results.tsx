5fa66e9734db21dbbcc25868fd1fd33eac0de3c6
Artur Iatsko <arturyatsko@gmail.com>
2022-10-08 16:42:29 -0400

    feat(results): get basic layout working based on viewport width

--- components/Results/Results.tsx
+++ components/Results/Results.tsx
@@ -1,36 +1,37 @@
 import {
   Center,
+  Group,
   MediaQuery,
   SegmentedControl,
   // Group,
   // Stack,
-  // Paper,
-  // Image,
-  // Badge,
-  // ActionIcon,
+  Paper,
+  Image,
+  Badge,
+  ActionIcon,
 } from '@mantine/core';
-// import PedalBikeIcon from '@mui/icons-material/PedalBike';
-// import ElectricBikeIcon from '@mui/icons-material/ElectricBike';
-// import DirectionsIcon from '@mui/icons-material/Directions';
-// import DockIcon from '@mui/icons-material/Dock';
+import PedalBikeIcon from '@mui/icons-material/PedalBike';
+import ElectricBikeIcon from '@mui/icons-material/ElectricBike';
+import DirectionsIcon from '@mui/icons-material/Directions';
+import DockIcon from '@mui/icons-material/Dock';
 import React, { useEffect, useState } from 'react';
-// import Map, { Marker, Popup } from 'react-map-gl';
+import Map, { Marker, Popup } from 'react-map-gl';
 // import { Marker } from 'react-map-gl';
 // import Station from '../Station/Station';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { useViewportSize } from '@mantine/hooks';
 
-// const returnIcon = (type: TSearchQuery['item']) => {
-//   if (type === 'bikes') return <PedalBikeIcon fontSize="small" />;
-//   if (type === 'e-bikes') return <ElectricBikeIcon fontSize="small" />;
-//   return <DockIcon fontSize="small" />;
-// };
+const returnIcon = (type: TSearchQuery['item']) => {
+  if (type === 'bikes') return <PedalBikeIcon fontSize="small" />;
+  if (type === 'e-bikes') return <ElectricBikeIcon fontSize="small" />;
+  return <DockIcon fontSize="small" />;
+};
 
-// const returnNumber = (type: TSearchQuery['item'], station: TStation) => {
-//   if (type === 'bikes') return station.num_bikes_available_types.mechanical;
-//   if (type === 'e-bikes') return station.num_bikes_available_types.ebike;
-//   return station.num_docks_available;
-// };
+const returnNumber = (type: TSearchQuery['item'], station: TStation) => {
+  if (type === 'bikes') return station.num_bikes_available_types.mechanical;
+  if (type === 'e-bikes') return station.num_bikes_available_types.ebike;
+  return station.num_docks_available;
+};
 
 type Props = {
   list?: TStation[];
@@ -39,8 +40,8 @@ type Props = {
 };
 
 export default function Results({ list, query, loading = false }: Props) {
-  const [display, setDisplay] = useState<string>('list');
-  // const [popup, setPopup] = useState<TStation | null>();
+  const [display, setDisplay] = useState<string>('stations');
+  const [popup, setPopup] = useState<TStation | null>();
   const { width } = useViewportSize();
   const [mode, setMode] = useState('full');
   useEffect(() => {
@@ -48,26 +49,71 @@ export default function Results({ list, query, loading = false }: Props) {
   }, [width]);
   console.log(loading, mode, list, query);
 
-  // const markers = list?.map((each, index) => (
-  //   <Marker
-  //     key={index}
-  //     longitude={each.lon}
-  //     latitude={each.lat}
-  //     onClick={(e) => {
-  //       e.originalEvent.stopPropagation();
-  //       setPopup(each);
-  //     }}
-  //   >
-  //     <Badge
-  //       size="md"
-  //       variant="filled"
-  //       leftSection={query && returnIcon(query.item)}
-  //       styles={() => ({ leftSection: { alignSelf: 'baseline' } })}
-  //     >
-  //       {query && returnNumber(query.item, each)}
-  //     </Badge>
-  //   </Marker>
-  // ));
+  const markers = list?.map((each, index) => (
+    <Marker
+      key={index}
+      longitude={each.lon}
+      latitude={each.lat}
+      onClick={(e) => {
+        e.originalEvent.stopPropagation();
+        setPopup(each);
+      }}
+    >
+      <Badge
+        size="md"
+        variant="filled"
+        leftSection={query && returnIcon(query.item)}
+        styles={() => ({ leftSection: { alignSelf: 'baseline' } })}
+      >
+        {query && returnNumber(query.item, each)}
+      </Badge>
+    </Marker>
+  ));
+
+  const map = (
+    <Paper withBorder style={{ overflow: 'hidden' }}>
+      <Map
+        initialViewState={{
+          longitude: query?.lng,
+          latitude: query?.lat,
+          zoom: 15,
+        }}
+        style={{ width: '100%', height: 500 }}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        attributionControl={false}
+      >
+        <Marker longitude={query?.lng} latitude={query?.lat}>
+          <Image src="/icons/current_location.svg" width={30} />
+        </Marker>
+
+        {markers}
+        {popup && (
+          <Popup
+            anchor="bottom"
+            longitude={popup.lon}
+            latitude={popup.lat}
+            onClose={() => setPopup(null)}
+            offset={15}
+            closeButton={false}
+          >
+            <ActionIcon
+              color="brandGreen"
+              size="md"
+              style={{ marginLeft: 'auto' }}
+              component="a"
+              aria-label="Navigate to station with Google Maps"
+              target="_blank"
+              href={`https://www.google.ca/maps/dir//${popup.lat},${popup.lon}/`}
+            >
+              <DirectionsIcon sx={{ fontSize: 25 }} />
+            </ActionIcon>
+          </Popup>
+        )}
+      </Map>
+    </Paper>
+  );
+  const stations = <div>Stations</div>;
 
   return (
     <>
@@ -79,12 +125,18 @@ export default function Results({ list, query, loading = false }: Props) {
             value={display}
             onChange={setDisplay}
             data={[
-              { label: 'List View', value: 'list' },
+              { label: 'List View', value: 'stations' },
               { label: 'Map View', value: 'map' },
             ]}
           />
         </Center>
       </MediaQuery>
+      <Group position="center" grow>
+        {width > 800 && map}
+        {width > 800 && stations}
+        {width < 800 && display === 'stations' && stations}
+        {width < 800 && display === 'map' && map}
+      </Group>
 
       {/* full-screen display */}
       {/* <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
