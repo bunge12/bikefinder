8b5a4f289e5f8ba83a783249827455217e218121
Artur Iatsko <arturyatsko@gmail.com>
2022-10-08 15:33:55 -0400

    feat(index): add response mocking to home page

--- stories/index.story.tsx
+++ stories/index.story.tsx
@@ -1,7 +1,17 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 import HomePage from '../pages/index';
+import { rest } from 'msw';
+import { stationsResponse } from '../components/data';
 
 export default {
   title: 'Pages/HomePage',
-};
+  component: HomePage,
+} as ComponentMeta<typeof HomePage>;
 
-export const Default = () => <HomePage />;
+const Template: ComponentStory<typeof HomePage> = () => <HomePage />;
+export const Default = Template.bind({});
+Default.parameters = {
+  msw: [
+    rest.post('api/bikeshare', (req, res, ctx) => res(ctx.delay(2000), ctx.json(stationsResponse))),
+  ],
+};
